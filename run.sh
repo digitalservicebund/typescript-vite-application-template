39466ccd3da3f571d85efeadf773e6476346bc33
Florian <florian.zechmeister@digitalservice.bund.de>
2022-04-05 17:22:10 +0200

    Add commit hooks

---
+++ run.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+set -eu
+
+readonly DEBUG="${DEBUG:-unset}"
+if [ "${DEBUG}" != unset ]; then
+  set -x
+fi
+
+_fail() {
+  printf "\033[0;31m==> %s\033[0m\n\n" "$1"
+}
+
+_success() {
+  printf "\033[0;32m==> %s\033[0m\n\n" "$1"
+}
+
+_info() {
+  printf "\033[1;33m==> %s\033[0m\n\n" "$1"
+}
+
+_user() {
+  printf "\033[0;33m%s\033[0m" "$1"
+}
+
+_setup_git_hooks() {
+  _user "Do you want to install the Git hooks ? (y/n) "
+  read -r answer
+  if [ "$answer" = "y" ]; then
+    if ! command -v lefthook > /dev/null 2>&1; then
+      _fail "Setup requires Lefthook, please install first: \`brew install lefthook\`"
+      exit 1
+    fi
+    if ! command -v talisman > /dev/null 2>&1; then
+      _fail "Setup requires Talisman, please install first: \`brew install talisman\`"
+      exit 1
+    fi
+    lefthook install
+    _info "Git hooks installed.."
+  fi
+}
+
+_init() {
+  _setup_git_hooks
+}
+
+_commit_message_template() {
+  template="$PWD/.git/.gitmessage.txt"
+  git config commit.template "$template"
+  cat << EOF > "$template"
+Subject
+
+Some context/description
+
+Addresses VER-${1#VER-}
+EOF
+}
+
+_help() {
+  echo "Usage: ./run.sh [command]"
+  echo ""
+  echo "Available commands:"
+  echo "init                  Set up repository for development"
+  echo "cm <issue-number>     Configure commit message template with given issue number;"
+  echo "                      issue number can be with or without prefix: 1234, VER-1234."
+}
+
+cmd="${1:-}"
+case "$cmd" in
+  "init") _init ;;
+  "cm")
+    shift
+    _commit_message_template "$@"
+    ;;
+  *) _help ;;
+esac
