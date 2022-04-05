e8f63c94fd691704d4c4228e71684a73daa41833
Florian <florian.zechmeister@digitalservice.bund.de>
2022-04-05 16:52:56 +0200

    Add jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ["js", "ts", "json"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/test/e2e"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/main.ts", // No need to cover bootstrap file
+    "!**/*.d.ts",
+  ],
+}
+
+export default config
