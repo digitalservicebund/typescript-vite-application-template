cab69aa78d39d53e44271cd9ff0182fdf01f44b1
Florian <florian.zechmeister@digitalservice.bund.de>
2022-04-06 10:53:21 +0200

    Add axe a11y testing

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,6 +1,11 @@
-import { expect, test } from "@playwright/test"
+import { test, expect, chromium, Browser, Page } from "@playwright/test"
+import { injectAxe, checkA11y } from "axe-playwright"
 
-test("basic test", async ({ page }) => {
-  await page.goto("http://localhost:4173/")
-  await expect(page.locator("text=DigitalService")).toBeVisible()
+test.describe("basic example test", () => {
+  test("basic test", async ({ page }) => {
+    await page.goto("http://localhost:4173/")
+    await injectAxe(page)
+    await expect(page.locator("text=DigitalService")).toBeVisible()
+    await checkA11y(page)
+  })
 })
