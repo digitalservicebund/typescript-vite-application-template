40eaafe06d612c24921968544eb792ab7912e697
Klaus Hartl <klaus.hartl@gmail.com>
2022-11-17 11:10:37 +0100

    Simplify playwright test setup
    We have the `baseURL` in the test config, thus we don't need to worry
    about host or port at all in the test.

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test"
 
 test.describe("basic example test", () => {
-  test("basic test", async ({ page }, { config }) => {
-    await page.goto(`http://localhost:${config.webServer?.port}`)
+  test("basic test", async ({ page }) => {
+    await page.goto("/")
     await expect(page.locator("text=DigitalService")).toBeVisible()
   })
 })
